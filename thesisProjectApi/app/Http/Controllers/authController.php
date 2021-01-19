d14eff679978846c0d9077e0f1d32fb5cb1e72a0
Dev-Yol <leonilojr.torres@student.passerellesnumeriques.org>
2021-01-19 17:51:58 +0800

    Modify inserting data to final collection

--- thesisProjectApi/app/Http/Controllers/authController.php
+++ thesisProjectApi/app/Http/Controllers/authController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Http\Request;
-use App\Models\cms_members;
+use App\Models\cms_users;
 use App\Models\cms_leaders;
+use App\Models\cms_accounts;
 
 class authController extends Controller
 {
     public function login(Request $request) {
-        $email=$request->input('email');
+        $email=$request->input('username');
         $pass=$request->input('password');
         
-        $userRequest=cms_members::where('Email', $email)
+        $userRequest=cms_accounts::where('username', $email)
         ->get();
-
+        
         $partialPassword=$userRequest->pluck('password');
         $password=Crypt::decryptString($partialPassword[0]);
         if($password == $pass) {
@@ -25,22 +26,65 @@ class authController extends Controller
     }
     public function signUp(Request $request) {
         $leader = new cms_leaders;
-        $user = new cms_members;
+        $user = new cms_users;
+        $newAccountCreate = new cms_accounts;
 
-        $user->age = $request->input('Age');
-        $user->leader = $request->input('Leader');
-        $user->member_status = $request->input('Member_status');
-        $user->email = $request->input('Email');
-        $user->name =  $request->input('Name');
-        $user->password = Crypt::encryptString($request->input('Password'));
+        $user->lastname = $request->newUser["Lastname"];
+        $user->firstname = $request->newUser["Firstname"];
+        $user->birthday = $request->newUser["Birthday"];
+        $user->age = $request->newUser["Age"];
+        $user->address = $request->newUser["Address"];
+        $user->marital_status = $request->newUser["Marital_status"];
+        $user->email = $request->newUser["Email"];
+        $user->contact_number = $request->newUser["Contact_number"];
+        $user->facebook = $request->newUser["Facebook"];
+        $user->instagram = $request->newUser["Instagram"];
+        $user->twitter = $request->newUser["Twitter"];
+        $user->leader = $request->groupBelong["Leader"];
+        $user->category = "Asian";
+        $user->isCGVIP = true;
+        $user->isSCVIP = true;
+        $user->auxilliary = "Romeo's Group";
+        $user->ministries = "Romeo's Ministry";
         $user->save();
-        if($user->member_status == "leader") {
-            $leader->user_id = $user->id;
-            $leader->pools_id = 1;
-            $leader->level = "3";
-            $leader->save();
-        }
+
+        $newUserId=$user->id;
+
+        $newAccountCreate->userid = $newUserId;
+        $newAccountCreate->username = $request->newUserAccount["username"];
+        $newAccountCreate->password = Crypt::encryptString($request->newUserAccount["password"]);
+        $newAccountCreate->roles = $request->role["code"];
+        $newAccountCreate->save();
+
         
-        return $leader;
+        return $newAccountCreate;
     }
 }
+
+// {
+//     "newUser": 
+//         {
+//             "Lastname": "Lenizo",
+//             "Firstname": "Romeo",
+//             "Birthday": "March 11, 1999",
+//             "Age": 21,
+//             "Address": "Dugyan",
+//             "Marital_status": "Single",
+//             "Email": "romeo@gmail.com",
+//             "Contact_number": "09959525960",
+//             "Facebook": "Romeo Lenizo",
+//             "Instagram": "null",
+//             "Twitter": "null"
+//         },
+//     "groupBelong": {
+//         "Leader": "Raymond Yorong"
+//     },
+//     "role" : {
+//             "code" : "member"
+//         },
+//     "newUserAccount" : {
+//         "username": "bai roms",
+//         "password": "123"
+//     }
+
+// }
