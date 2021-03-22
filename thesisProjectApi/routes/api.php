cf7f9167786da2135bae9938315ad13359868057
Dev-Yol <leonilojr.torres@student.passerellesnumeriques.org>
2021-03-22 21:29:51 +0800

    Initial Push

--- thesisProjectApi/routes/api.php
+++ thesisProjectApi/routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('get-leaders', 'UserDisplayController@getAllLeaders');
+
 //This is for looking who is active and inactive users
 Route::get('allMemberUsers', 'findActiveAndInactiveUsers@returnAllMembers');
 
