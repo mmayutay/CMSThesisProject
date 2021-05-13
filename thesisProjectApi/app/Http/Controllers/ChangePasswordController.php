c5de8ea9a85fb47c044c9caa975055c61f67ab3d
MissQAssociates <msqintern0@gmail.com>
2021-05-13 11:36:10 +0800

    Initial Commit

--- thesisProjectApi/app/Http/Controllers/ChangePasswordController.php
+++ thesisProjectApi/app/Http/Controllers/ChangePasswordController.php
@@ -4,60 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Symphony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
 
-use App\Models\cms_acconts;
+use Illuminate\Support\Facades\Crypt;
+use App\Models\cms_accounts;
+use App\Models\recover_passwords;
 use App\Http\Requests\UpdatePasswordRequest;
 
 class ChangePasswordController extends Controller
 {
     //
-    public function passwordResetProcess(UpdatePasswordRequest $request) {
-        return $this->updatePasswordRow($request)->count() > 0 ? $this->resetPassword($request) : $this->codeNotFoundError();
-    }
+    public function resetPassword(Request $request)
+    {
+        try {
+            $value = $request->currpassword;
 
+            $user = cms_accounts::where(
+                'username',
+                $request->username
+            )->first();
 
-    //Verify if code is valid
-    private function updatePasswordRow(Request $request) {
-        return DB::table('recover_password')->where([
-            'username' => $request->email,
-            'code' => $request->passCode
-        ]);
-        // $data = $request->validate([
-        //     'username' => 'requiresd|string',
-        //     'code'=> 'required|string',
-        // ]);
+            if (!is_null($user)) {
+                // $val =Crypt::decryptString($user->password);
+                $password = \Hash::check($value, $user[0]->password);
+                if ($password) {
+                    $user->password = Crypt::encryptString(
+                        $request->newPassword
+                    );
+                    $user->save();
 
-        // $holder = DB::table('recover_password')->where([
-        //         'username' => $request->email,
-        //         'code' => $request->passCode
-        //     ]);
-
-        // $count = count($holder);
-
-        // if() {
-
-        // }
-    }
-
-    //Token not found response
-    private function codeNotFoundError() {
-        return response()->json([
-            'error' => 'Username or code is wrong'
-        ],Response::HTTP_UNPROCESSABLE_ENTIY);
-    }
-
-    //Reset password response
-    private function resetPassword($request) {
-
-        $userData = cms_accounts::where('username', $request->username)->first();
-
-        $userData->update([
-            'password' => bcrypt($request->password),
-        ]);
-
-        return response()->json([
-            'data' => 'Password has been updated'
-        ], Response::HTTP_CREATED);
+                    return $user;
+                }
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'error',
+                'Username or Password is incorrect ' . $e->getMessage(),
+            ]);
+        }
     }
 }
