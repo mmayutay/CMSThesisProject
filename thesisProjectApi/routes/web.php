3c70f9bf6ab943e5c014984d89f0dd989288a808
MissQAssociates <msqintern0@gmail.com>
2021-05-13 15:39:49 +0800

    fix index

--- thesisProjectApi/routes/web.php
+++ thesisProjectApi/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Hello world';
+    return 'Hello worldasfsafA';
 });
 // Route::get('/info','UserDisplayController@index');
 
